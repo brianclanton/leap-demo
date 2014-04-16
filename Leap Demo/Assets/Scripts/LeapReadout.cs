cc18effa19ec85dbcbce7d0a5dbf028971a6f5f0
unknown <bdc1710@GDD-44.main.ad.rit.edu>
2014-04-16 10:53:53 -0400

    GUIText readouts will be created as children of ReadoutController and updated in an attached script via the update method.

--- Leap Demo/Assets/Scripts/LeapReadout.cs
+++ Leap Demo/Assets/Scripts/LeapReadout.cs
@@ -4,18 +4,45 @@ using Leap;
 
 public class LeapReadout : MonoBehaviour {
 
-	public GUIText test;
+	public GUIText readoutPrefab;
+
+	private GUIText controllerStatus;
+	private GUIText test1;
+	private GUIText test2;
+
+	private float baseOffsetX = 0.95f;
+	private float baseOffsetY = 0.95f;
+	private float spacing = 0.1f;
 
 	private Controller controller;
+
+	private bool isConnected = false;
 
 	// Use this for initialization
 	void Start () {
 		controller = new Controller();
+		controllerStatus = transform.Find("ControllerStatus").GetComponent<GUIText>();
+		test1 = transform.Find("Test1").GetComponent<GUIText>();
+		test2 = transform.Find("Test2").GetComponent<GUIText>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Frame frame = controller.Frame();
-		test.text = "" + Time.deltaTime;
+		// Update connection status of LeapMotion device
+		if (!isConnected && controller.IsConnected) {
+			isConnected = true;
+			controllerStatus.text = "Leap Motion connected";
+		} else if (isConnected && !controller.IsConnected) {
+			isConnected = false;
+			controllerStatus.text = "Leap Motion disconnected";
+		}
+
+		if (isConnected) {
+			Frame frame = controller.Frame();
+		}
+
+		fps.text = "" + Time.deltaTime;
+		test1.text = "" + Time.frameCount;
+		test2.text = "" + Time.realtimeSinceStartup;
 	}
 }
